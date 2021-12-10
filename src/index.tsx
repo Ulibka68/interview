b53c2fb2f06d2c70850eb9b938c3d91f50e46b02
Ulibka68 <gena68@inbox.ru>
2021-12-10 13:20:43 +0300

    react boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+    <div>
+      <h1>React boilerplate</h1>
+    </div>  ,
+  document.getElementById('root')
+);
